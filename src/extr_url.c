53d6264f97230d355491afc6fccfea1bfaed0222
fierceboy <zcrzcr1988@163.com>
2018-09-24 21:02:59 +0800

    add

--- src/extr_url.c
+++ src/extr_url.c
@@ -2,15 +2,37 @@
 #define _GNU_SOURCE
 #include <string.h>
 
+int tld_check()
+{
+	return 0;
+}
+
+int extr_tld_after_dot()
+{
+
+}
+
 int extr_url_from_text(char *text, int text_len, url_set_st *url_set)
 {
 	//It is impossible contain URL in str.
 	if(text == NULL || text_len < MIN_URL_LEN)
 		return 0;
 
+	int ret = 0;
+	int tld_length = 0;
+
 	//find a domain in text
-	strcasestr(text, "http://") != NULL
-	strcasestr(text, "https://") != NULL;
+	//strcasestr(text, "http://") != NULL
+	//strcasestr(text, "https://") != NULL;
+	//find TLD in text
+	while(text_len){
+		if(*text == '.'){
+			//find tld after '.'
+			ret = extr_tld_after_dot(text+1, text_len-1, &tld_length);
+		}
+		text_len --;
+		text ++;
+	}
 	
 	return 0;
 }
